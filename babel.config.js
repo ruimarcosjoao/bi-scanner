432d428175211afdf37a4f5f2342081be6983319
Rui Marcos João <ruimarcosjoao@gmail.com>
2024-02-02 15:45:54 +0100

    feat: add the scanner qr code to read bi data

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,14 @@
 module.exports = function (api) {
   api.cache(true);
-  const plugins = [];
-
   return {
-    presets: [['babel-preset-expo', { jsxImportSource: 'nativewind' }], 'nativewind/babel'],
-
-    plugins,
+    presets: ['babel-preset-expo'],
+    "plugins": [
+      [
+        "expo-camera",
+        {
+          "cameraPermission": "Allow $(PRODUCT_NAME) to access your camera."
+        }
+      ]
+    ]
   };
 };
